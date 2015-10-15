eda5253b8a08540a950154fd5523b4e36a9abf33
Rafal <rkpieniazek@gmail.com>
2015-10-15 10:30:41 +0200

    dwa

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -32,9 +32,10 @@
             this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,11 +50,12 @@
             // 
             // button1
             // 
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button1.Location = new System.Drawing.Point(56, 53);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(162, 90);
+            this.button1.Size = new System.Drawing.Size(162, 55);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Play";
+            this.button1.Text = "Play ActiveX";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -66,16 +68,6 @@
             this.button2.Text = "Stop";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(56, 183);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(162, 71);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Pauza";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // textBox1
             // 
@@ -95,14 +87,35 @@
             this.label1.Text = "Nazwa utworu:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(58, 114);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(160, 71);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "DirectSound";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(58, 191);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(160, 52);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "WaveOut";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(828, 479);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.axWindowsMediaPlayer1);
@@ -119,9 +132,10 @@
         private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -59,5 +59,15 @@ namespace WindowsFormsApplication1
         {
 
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
